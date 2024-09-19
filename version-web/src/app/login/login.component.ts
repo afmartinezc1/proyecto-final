f30d71e27b9cc1764af3eb21cb04796ba8e3d3aa
Andres Martinez <af.martinezc1@uniandes.edu.co>
2024-09-18 23:22:17 -0500

    Primer comit

---
+++ version-web/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent {}
