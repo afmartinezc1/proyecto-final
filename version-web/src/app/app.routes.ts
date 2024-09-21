ac57b0872d75f7e11b1c6e32da47723f574cb48b
Fabian Orozco <fabian.orozco@mercadolibre.com.co>
2024-09-20 23:11:43 -0500

    feature: carga inicial

--- version-web/src/app/app.routes.ts
+++ version-web/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CrearRecordatorioMedicamentoComponent } from './crear-recordatorio-medicamento/crear-recordatorio-medicamento.component';
+import { CargarOrdenMedicaComponent } from './cargar-orden-medica/cargar-orden-medica.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'crear-recordatorio', component: CrearRecordatorioMedicamentoComponent},
+  { path: 'cargar-orden', component: CargarOrdenMedicaComponent}
 ];
