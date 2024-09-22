1e68598769c7245af93aaf6015d3fb3c14500486
Andres Martinez <af.martinezc1@uniandes.edu.co>
2024-09-21 19:13:58 -0500

    Ajuste back button

--- version-movil/src/app/configuracion/configuracion.page.ts
+++ version-movil/src/app/configuracion/configuracion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonRouterOutlet } from '@ionic/angular';
 
 @Component({
   selector: 'app-configuracion',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfiguracionPage implements OnInit {
 
-  constructor() { }
+  constructor(private routerOutlet: IonRouterOutlet) { }
 
   ngOnInit() {
   }
 
+  goBack() { this.routerOutlet.pop(); }
 }
